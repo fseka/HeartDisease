a561545794ff453d573b47fbfa68580b4cf659fa
Florian Seka <florian@mbp-de-florian.mynet>
2019-05-17 14:44:36 +0100

    Updated the analyssis report with transformation of features into proper formats (numeric, factors with levels, etc...)

--- HDAnalysis.R
+++ HDAnalysis.R
@@ -1,5 +1,9 @@
 ##Analysis script
 
+# Cleaning workspace
+rm(list=ls())
+
+# Loading the used libraries
 library(tidyverse)
 library(caret)
 library(ggthemes)
@@ -29,6 +33,7 @@ hungariandata<- mutate (hungariandata, institute="Hungary")
 # build a unique dataset concatenating the rows from all previous 4 research institutes
 heartdisease<-rbind(clevelanddata,swissdata,vadata,hungariandata)
 
+
 # We can now remove the individual datasets and variables to free some workspace memory
 rm(clevelanddata,swissdata,vadata,hungariandata,datacolnames,datasetdir)
 
@@ -37,10 +42,61 @@ rm(clevelanddata,swissdata,vadata,hungariandata,datacolnames,datasetdir)
 
 ## Data preparation
 
+
+# Adding factors to the features based on the data description. This will facilitate the creation and labelling of plots with the ggplot() package
+heartdisease$sex<-as.factor(heartdisease$sex)
+heartdisease$fbs<-as.factor(as.numeric(as.character(heartdisease$fbs)))
+heartdisease$exang<-as.factor(as.numeric(as.character(heartdisease$exang)))
+
+heartdisease$restecg<-as.factor(as.numeric(heartdisease$restecg))
+heartdisease$slope<-as.factor(as.numeric(as.character(heartdisease$slope)))
+heartdisease$thal<-as.factor(as.numeric(as.character(heartdisease$thal))) 
+heartdisease$ca<-as.factor(as.numeric(as.character(heartdisease$ca)))
+heartdisease$cp<-as.factor(as.numeric(as.character(heartdisease$cp)))
+
+heartdisease$institute<-as.factor(as.character(heartdisease$institute))
+
+levels(heartdisease$sex)[levels(heartdisease$sex)==0] <- "Female"
+levels(heartdisease$sex)[levels(heartdisease$sex)==1] <- "Male"
+
+levels(heartdisease$fbs)[levels(heartdisease$fbs)==0] <- "Fasting Blood Sugar <= 120"
+levels(heartdisease$fbs)[levels(heartdisease$fbs)==1] <- "Fasting Blood Sugar > 120"
+
+levels(heartdisease$exang)[levels(heartdisease$exang)==0] <- "No Exercise Induced Angina"
+levels(heartdisease$exang)[levels(heartdisease$exang)==1] <- "Exercise Induced Angina"
+
+levels(heartdisease$restecg)[levels(heartdisease$restecg)==0] <- "REST ECG 0"
+levels(heartdisease$restecg)[levels(heartdisease$restecg)==1] <- "REST ECG 1"
+levels(heartdisease$restecg)[levels(heartdisease$restecg)==2] <- "REST ECG 2"
+
+levels(heartdisease$cp)[levels(heartdisease$cp)==1] <- "Chest Pain type 1"
+levels(heartdisease$cp)[levels(heartdisease$cp)==2] <- "Chest Pain type 2"
+levels(heartdisease$cp)[levels(heartdisease$cp)==3] <- "Chest Pain type 3"
+levels(heartdisease$cp)[levels(heartdisease$cp)==4] <- "Chest Pain type 4"
+
+levels(heartdisease$slope)[levels(heartdisease$slope)==1] <- "Peak Exercise Slope Upsloping"
+levels(heartdisease$slope)[levels(heartdisease$slope)==2] <- "Peak Exercise Slope Flat"
+levels(heartdisease$slope)[levels(heartdisease$slope)==3] <- "Peak Exercise Slope Downsloping"
+
+levels(heartdisease$thal)[levels(heartdisease$thal)==3] <- "Thalium ST normal"
+levels(heartdisease$thal)[levels(heartdisease$thal)==6] <- "Fixed defect"
+levels(heartdisease$thal)[levels(heartdisease$thal)==7] <- "Reversible defect"
+
 # Adding the presence boolean column: the binary heart disease presence column is built based on the presence column and is a simplification of the obersvation.
 # While the presence takes integer values from 0 to 4 (0 being the absolute absence of disease), this new value presence will indicate presence (presence=1,2,3or4) or absence (presence=0).
 
-heartdisease <- mutate(heartdisease,presence=!(target==0))
+heartdisease <- mutate(heartdisease,presence=1*!(target==0))
+
+# Adding levels for the heart disease presence column
+heartdisease$presence<-as.factor(heartdisease$presence)
+levels(heartdisease$presence)[levels(heartdisease$presence)==0] <- "Healthy"
+levels(heartdisease$presence)[levels(heartdisease$presence)==1] <- "Heart Disease"
+
+# Converting numerical data to numeric format
+heartdisease$thalach<-as.numeric(heartdisease$thalach)
+heartdisease$trestbps<-as.numeric(heartdisease$trestbps)
+heartdisease$chol<-as.numeric(heartdisease$chol)
+heartdisease$oldpeak<-as.numeric(heartdisease$oldpeak)
 
 # Let's first visualize the distribution of patients depending on the contributing research institute
 heartdisease %>% group_by(institute) %>% 
@@ -79,9 +135,31 @@ hd_trainset %>%
   ylab("Density") + 
   scale_fill_manual(name="Disease", labels=c("Absent","Present"),values=c("springgreen2","firebrick2"))
 
-
-#[TODO]: ajouter le notion de levels pour que les plots puissent se faire
 #Sex
-hd_trainset %>% filter (presence==TRUE) %>%
+hd_trainset %>% filter (presence=="Heart Disease") %>%
   ggplot(aes(age,fill=sex)) +
   geom_density(alpha = 0.4)
+
+#Cholesterol
+hd_trainset %>% 
+  ggplot(aes(chol,fill=presence)) +
+  geom_density(alpha = 0.4) +
+  facet_wrap(hd_trainset$institute, ncol=1,scale="free_y")
+
+#Trestbps Resting blood pressure (in mm Hg on admission to the hospital)
+hd_trainset %>% 
+  ggplot(aes(trestbps,fill=presence)) +
+  geom_density(alpha = 0.4) +
+  facet_wrap(hd_trainset$institute, ncol=1,scale="free_y")
+
+# Thalach maximum heart rate achieved 
+hd_trainset %>% 
+  ggplot(aes(thalach,fill=presence)) +
+  geom_density(alpha = 0.4) +
+  facet_wrap(hd_trainset$institute, ncol=1,scale="free_y")
+
+# Oldpeak ST depression induced by exercise relative to rest 
+hd_trainset %>% 
+  ggplot(aes(oldpeak,fill=presence)) +
+  geom_density(alpha = 0.4) +
+  facet_wrap(hd_trainset$institute, ncol=1,scale="free_y")
